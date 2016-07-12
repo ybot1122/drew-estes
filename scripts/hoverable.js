297ea3a77725698ca7075285fd06c6487c64576d
Toby Liu <ybot1122@gmail.com>
2016-07-11 20:22:26 -0700

    created stub to handle hover pseudo class

---
+++ scripts/hoverable.js
@@ -0,0 +1,9 @@
+var hoverableEnabled = true;
+
+if (hoverableEnabled) {
+  var cards = document.getElementsByClassName("card");
+  for (var i = 0; i < cards.length; i++) {
+    console.log(cards[i].className);
+    cards[i].className += " hoverable";
+  }
+}
