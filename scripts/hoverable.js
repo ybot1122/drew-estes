d0d621193af4015ad29613ab7b0af8c0d8a3cee3
Toby Liu <ybot1122@gmail.com>
2016-07-15 21:53:10 -0700

    routine async

--- scripts/hoverable.js
+++ scripts/hoverable.js
@@ -1,13 +1,8 @@
 // http://www.stucox.com/blog/you-cant-detect-a-touchscreen/#poke-it
-var hasTouch;
 window.addEventListener('touchstart', function setHasTouch () {
-    hasTouch = true;
+    var cards = document.getElementsByClassName("card");
+    for (var i = 0; i < cards.length; i++) {
+      cards[i].className = cards[i].className.replace(" hoverable", "");
+    }
     window.removeEventListener('touchstart', setHasTouch);
 }, false);
-
-if (hasTouch) {
-  var cards = document.getElementsByClassName("card");
-  for (var i = 0; i < cards.length; i++) {
-    cards[i].className = cards[i].className.replace(" hoverable", "");
-  }
-}
