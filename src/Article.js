7721eeea5e74de5cabe689ef91be328cf560f1e7
Toby Liu <ybot1122@gmail.com>
2017-01-01 14:30:43 -0800

    image sources

--- src/Article.js
+++ src/Article.js
@@ -42,9 +42,16 @@ class Article extends Component {
     const result = [];
     for (let i = 0; i < data.length; i++) {
       if (data[i].node === 'element') {
-        if (data[i].attr && data[i].attr.class) {
-          data[i].attr.className = data[i].attr.class;
-          delete data[i].attr.class;
+        if (data[i].attr) {
+          // replace class attribute with className
+          if (data[i].attr.class) {
+            data[i].attr.className = data[i].attr.class;
+            delete data[i].attr.class;
+          }
+          // prepend PUBLIC_URL to image sources
+          if (data[i].tag === 'img' && data[i].attr.src) {
+            data[i].attr.src = process.env.PUBLIC_URL + '../' + data[i].attr.src;
+          }
         }
         const children = this._parseHtmlTree(data[i].child);
         const props = Object.assign({}, data[i].attr, { key: i });
