bd171e97c7669ba2007d9db0b746bddb848344d6
Toby Liu <ybot1122@gmail.com>
2017-04-09 10:22:03 -0700

    add some clickability to patreon link

--- src/SupportPatreon.js
+++ src/SupportPatreon.js
@@ -6,8 +6,9 @@ class SupportPatreon extends Component {
   render() {
     return (
       <div className="article">
-        <h3>Support on Patreon</h3>
-        <a href="https://www.patreon.com/quackrabbit"><img id="patreon-logo" src={patreonLogo} alt="patreon logo" /></a>
+        <h3><a href="https://www.patreon.com/quackrabbit" target="_blank">Support QuackRabbit on Patreon!</a></h3>
+        <p>Pledge today at <a href="https://www.patreon.com/quackrabbit" target="_blank">https://www.patreon.com/quackrabbit</a>.</p>
+        <a href="https://www.patreon.com/quackrabbit"><img id="patreon-logo" src={patreonLogo} alt="patreon logo" target="_blank" /></a>
       </div>
     );
   }
