70796200c1291af70febb2584cd13950beab7452
Toby Liu <toby.liu@hulu.com>
2018-02-16 12:42:18 -0800

    implement get and download as html

---
+++ gdrive/src/main/java/Quickstart.java
@@ -0,0 +1,121 @@
+// https://developers.google.com/drive/v3/web/quickstart/java
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Quickstart {
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+            "QuackRabbit Article Publisher";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+                Quickstart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Drive client service.
+     * @return an authorized Drive client service
+     * @throws IOException
+     */
+    public static Drive getDriveService() throws IOException {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileId = "1kjrIpu3Yv0ToeZCFHA49LLSDAXSkQUBGvO7_yFpEzJ4";
+
+        // Build a new authorized API client service.
+        Drive service = getDriveService();
+
+        // Get the file by ID
+        File result = service.files().get(fileId).execute();
+        System.out.println("Title: " + result.getName());
+        System.out.println("Description: " + result.getDescription());
+        System.out.println("MIME type: " + result.getMimeType());
+
+        // Download it as HTML
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        FileOutputStream fileStream = new FileOutputStream(fileId + ".html");
+        service.files().export(fileId, "text/html").executeMediaAndDownloadTo(outputStream);
+        outputStream.writeTo(fileStream);
+        outputStream.close();
+        fileStream.close();
+        System.out.println("Downloaded html as " + fileId + ".html");
+    }
+
+}
