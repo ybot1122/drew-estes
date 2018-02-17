1da9c0bebacf3b5b570a7e8384a05b6b1ea56005
Toby Liu <ybot1122@gmail.com>
2018-02-17 10:10:48 -0800

    soem updates

--- src/Admin.js
+++ src/Admin.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
+import { g_API_KEY, g_CLIENT_ID } from './utils/constants/CREDENTIALS';
 
-const g_API_KEY = '*';
-const g_CLIENT_ID = '*.apps.googleusercontent.com';
 const g_SCOPES = 'profile https://www.googleapis.com/auth/drive';
 const g_DISCOVER_DOCS = ['https://developers.google.com/drive/v3/reference/'];
 
@@ -23,10 +22,13 @@ class Admin extends Component {
     this.initializeGoogleApi = this.initializeGoogleApi.bind(this);
     this.updateSigninStatus = this.updateSigninStatus.bind(this);
     this.handleAuthClick = this.handleAuthClick.bind(this);
+    this.handlePublishSubmit = this.handlePublishSubmit.bind(this);
     this.state = {
-      isGoogleApiInitialized: false,
       loginStatus: 0, // 0: never tried, 1: in progress, 2: success, 3: fail
+      publishRequestStatus: 0, // 0: never tried, 1: in progress, 2: success, 3: fail
+      isGoogleApiInitialized: false,
       googleUserData: {},
+      googleDocId: '',
     };
   }
 
@@ -49,11 +51,13 @@ class Admin extends Component {
     if (!this.state.isGoogleApiInitialized) {
       promiseChain = this.initializeGoogleApi();
     }
+    console.log(promiseChain);
     promiseChain.then(() => {
       const gapi = window.gapi;
       this.setState({ loginStatus: 1 });
       gapi.auth2.getAuthInstance().signIn().then(() => {
         const currGoogleUser = gapi.auth2.getAuthInstance().currentUser.get();
+        gapi.auth2.getAuthInstance().currentUser.listen(this.updateSigninStatus);
         this.setState({
           loginStatus: 2,
           googleUserData: {
@@ -62,7 +66,6 @@ class Admin extends Component {
             authResponse: currGoogleUser.getAuthResponse(true),
           },
         });
-        gapi.auth2.getAuthInstance().currentUser.listen(this.updateSigninStatus);
       }).catch((e) => {
         console.error(e);
         this.setState({ loginStatus: 3 });
@@ -80,7 +83,7 @@ class Admin extends Component {
               discoveryDocs: g_DISCOVER_DOCS,
               clientId: g_CLIENT_ID,
               scope: g_SCOPES
-          }).then(() => resolve(true));
+          }).then(() => resolve(true), (e) => reject(e));
         });
       } catch(e) {
         reject('google api never loaded successfully. kill this page. ' + e);
@@ -88,13 +91,33 @@ class Admin extends Component {
     });
   }
 
-  render() {
+  handlePublishSubmit() {
+    this.setState({ publishRequestStatus: 1 });
+    setTimeout(() => {
+      const reqBody = {
+        token: this.state.googleUserData.authResponse,
+        docId: this.state.googleDocId,
+      };
+      this.setState({ publishRequestStatus: 2 });
+      console.log(reqBody);
+    }, 1000);
+  }
 
+  render() {
     let cta = (<a href="#" onClick={this.handleAuthClick}>Click here to log in to your Google Drive</a>);
     if (this.state.loginStatus === 1) {
       cta = (<span>LOADING</span>);
     } else if (this.state.loginStatus === 2) {
-      cta = (<span>WELCOME {this.state.googleUserData.name}</span>);
+      return (
+        <div className="container-fluid">
+          <h1>WELCOME {this.state.googleUserData.name}</h1>
+          <p>
+            Enter the ID of the Google Doc you want to publish:
+            <input type="text" value={this.state.googleDocId} onChange={(event) => this.setState({ googleDocId: event.value })} />
+            <input type="submit" value="Submit" onClick={this.handlePublishSubmit} />
+          </p>
+        </div>
+      );
     } else if (this.state.loginStatus === 3) {
       cta = (<a href="#" onClick={this.handleAuthClick}>Something went wrong, try again.</a>);
     }
